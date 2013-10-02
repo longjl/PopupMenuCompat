b92e68dd41a57dc55a8db7d96ce9734fc2518d6f
zhangxiaoke <zhangxiaoke@douban.com>
2013-10-02 23:53:22 +0800

    add project files

---
+++ samples/src/main/java/com/mcxiaoke/popupmenucompat/samples/Samples.java
@@ -0,0 +1,16 @@
+package com.mcxiaoke.popupmenucompat.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ */
+public class Samples extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ tests/src/main/java/com/mcxiaoke/popupmenucompat/tests/SamplesTest.java
@@ -0,0 +1,37 @@
+package com.mcxiaoke.popupmenucompat.tests;
+
+import com.mcxiaoke.popupmenucompat.samples.Samples;
+import android.test.ActivityInstrumentationTestCase2;
+import com.jayway.android.robotium.solo.Solo;
+
+/**
+ * User: mcxiaoke
+ * Date: 13-10-1
+ * Time: 上午11:25
+ */
+public class SamplesTest extends ActivityInstrumentationTestCase2<Samples> {
+    private Solo solo;
+
+    public SamplesTest() {
+        super(Samples.class);
+    }
+
+    public void testClass() {
+        assertEquals(Samples.class, getActivity().getClass());
+    }
+
+    public void testa() {
+
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
